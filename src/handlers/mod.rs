8eb332c99524bfacea1af73871ed816a0492e166
jglara <jglara@gmail.com>
2024-01-12 14:23:46 +0100

    Stage 3 finished

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -1,40 +1,91 @@
-use rocket::{serde::json::Json};
+use crate::persistance::answers_dao::AnswersDao;
+use crate::persistance::questions_dao::QuestionsDao;
+use rocket::{serde::json::Json, State};
+use serde::de;
 
 use crate::models::*;
+
+mod handlers_inner;
+
+use handlers_inner::*;
+
+#[derive(Responder)]
+pub enum APIError {
+    #[response(status = 400)]
+    BadRequest(String),
+    #[response(status = 500)]
+    InternalError(String),
+}
+
+impl From<HandlerError> for APIError {
+    fn from(value: HandlerError) -> Self {
+        match value {
+            HandlerError::BadRequest(s) => Self::BadRequest(s),
+            HandlerError::InternalError(s) => Self::InternalError(s),
+        }
+    }
+}
 
 // ---- CRUD for Questions ----
 
 #[post("/question", data = "<question>")]
 pub async fn create_question(
     question: Json<Question>,
-) -> Json<QuestionDetail> {
-    todo!()
+    questions_dao: &State<Box<dyn QuestionsDao + Sync + Send>>,
+) -> Result<Json<QuestionDetail>, APIError> {
+
+    let detail = handlers_inner::create_question(question.0, questions_dao).await?;
+
+    Ok(Json(detail))
 }
 
 #[get("/questions")]
-pub async fn read_questions() -> Json<Vec<QuestionDetail>> {
-    todo!()
+pub async fn read_questions(
+    questions_dao: &State<Box<dyn QuestionsDao + Sync + Send>>,
+) -> Result<Json<Vec<QuestionDetail>>, APIError> {
+    let details = handlers_inner::read_questions(questions_dao).await?;
+
+    Ok(Json(details))
 }
 
 #[delete("/question", data = "<question_uuid>")]
 pub async fn delete_question(
-    question_uuid: Json<QuestionId>
-) {
-    todo!()
+    question_uuid: Json<QuestionId>,
+    questions_dao: &State<Box<dyn QuestionsDao + Sync + Send>>,
+) -> Result<(), APIError> {
+    handlers_inner::delete_question(question_uuid.0, questions_dao).await?;
+
+    Ok(())
 }
 
 // ---- CRUD for Answers ----
 #[post("/answer", data = "<answer>")]
-pub async fn create_answer(answer: Json<Answer>) -> Json<AnswerDetail> {
-    todo!()
+pub async fn create_answer(
+    answer: Json<Answer>,
+    answers_dao: &State<Box<dyn AnswersDao + Sync + Send>>,
+) -> Result<Json<AnswerDetail>, APIError> {
+    let detail = handlers_inner::create_answer(answer.0, answers_dao).await?;
+
+    Ok(Json(detail))
 }
 
-#[get("/answers")]
-pub async fn read_answers() -> Json<Vec<AnswerDetail>> {
-    todo!()
+#[get("/answers", data= "<question_uuid>")]
+pub async fn read_answers(
+    question_uuid: Json<QuestionId>,
+    answers_dao: &State<Box<dyn AnswersDao + Sync + Send>>,
+) -> Result<Json<Vec<AnswerDetail>>, APIError> {
+    
+    let details = handlers_inner::read_answers(question_uuid.0, answers_dao).await?;
+
+    Ok(Json(details))
 }
 
 #[delete("/answer", data = "<answer_uuid>")]
-pub async fn delete_answer(answer_uuid: Json<AnswerId>) {
-    todo!()
+pub async fn delete_answer(
+    answer_uuid: Json<AnswerId>,
+    answers_dao: &State<Box<dyn AnswersDao + Sync + Send>>,
+) -> Result<(), APIError> {
+    handlers_inner::delete_answer(answer_uuid.0, answers_dao).await?;
+
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ extern crate pretty_env_logger;
 
 use dotenvy::dotenv;
 
+use persistance::answers_dao;
 use sqlx::postgres::PgPoolOptions;
 
 mod cors;
@@ -17,6 +18,9 @@ mod persistance;
 
 use cors::*;
 use handlers::*;
+
+use persistance::answers_dao::{AnswersDao, AnswersDaoImpl};
+use persistance::questions_dao::{QuestionsDao, QuestionsDaoImpl};
 
 #[launch]
 async fn rocket() -> _ {
@@ -28,8 +32,10 @@ async fn rocket() -> _ {
         .connect(&std::env::var("DATABASE_URL").expect("DATABASE_URL must be set."))
         .await
         .expect("Failed to create Postgres connection pool!");
-    
 
+    let questions_dao: Box<dyn QuestionsDao + Send + Sync> =
+        Box::new(QuestionsDaoImpl::new(pool.clone()));
+    let answers_dao: Box<dyn AnswersDao + Send + Sync> = Box::new(AnswersDaoImpl::new(pool));
 
     rocket::build()
         .mount(
@@ -44,4 +50,6 @@ async fn rocket() -> _ {
             ],
         )
         .attach(CORS)
+        .manage(questions_dao)
+        .manage(answers_dao)
 }
